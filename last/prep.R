bd459a356211ca98865fd5fd49d43a273d1ce5a8
tiagombp <tiago.pereira@tesouro.gov.br>
2025-01-30 04:44:54 -0300

    organizacao do repositório

---
+++ last/prep.R
@@ -0,0 +1,155 @@
+library(readxl)
+library(tidyverse)
+
+
+# 01a Importa Receitas do RGPS --------------------------------------------
+
+rec_rgps_raw <- read_excel("./dados/rec_rgps.xlsx", skip = 10)
+
+colnames(rec_rgps_raw) <- c(
+  "especie_cod", "especie",
+  "desdobramento_cod", "desdobramento",
+  "natureza_cod", "natureza",
+  "fte_cod", "fte",
+  "valor_rec"
+)
+
+rec_rgps <- rec_rgps_raw %>%
+  mutate(receita = "RGPS")
+
+
+# 01b Importa Demais Receitas ---------------------------------------------
+
+rec_raw <- read_excel("./dados/rec.xlsx", skip = 10)
+
+colnames(rec_raw) <- c(
+  "especie_cod", "especie",
+  "desdobramento_cod", "desdobramento",
+  "natureza_cod", "natureza",
+  "fte_cod", "fte",
+  "valor_rec"
+)
+
+# tab_rec <- rec_raw %>%
+#   mutate(cod_esp = str_sub(natureza_cod, 1, 3),
+#          nom_esp = especie) %>%
+#   select(cod_esp, nom_esp) %>%
+#   filter(str_sub(cod_esp, 1, 1) %in% c("1", "2")) %>%
+#   distinct()
+
+
+rec <- rec_raw %>%
+  bind_rows(rec_rgps) %>%
+  mutate(
+    cod_esp  = str_sub(natureza_cod, 1, 3),
+    cat      = str_sub(natureza_cod, 1, 1) %>% as.numeric(),
+    comp_esp = str_sub(natureza_cod, 2, 3),
+    
+    cod_esp = ifelse(
+      cat >= 7, 
+      paste0(cat-6, comp_esp),
+      cod_esp),
+    
+    receita = case_when(
+      receita == "RGPS"                   ~ "Receitas do RGPS",
+      cod_esp %in% c("211", "212")        ~ "Emissões de Dívida",
+      cod_esp %in% c("132", "164", "230") ~ "Juros e remunerações recebidas",
+      cod_esp == "111"                    ~ "Impostos",
+      cod_esp %in% c("121", "122")        ~ "Contribuições sociais (exceto RGPS) e econômicas",
+      cod_esp == "134"                    ~ "Exploração de Petróleo e outros recursos naturais",
+      cod_esp %in% c("292", "293")        ~ "Outras receitas financeiras",
+      TRUE                                ~ "Demais receitas"
+    )
+  )
+  
+rec %>% group_by(receita) %>% summarise(sum(valor_rec))
+
+# 01c Importa Despesas ----------------------------------------------------
+
+desp <- read_excel("./dados/desp.xlsx", skip = 10)
+
+colnames(desp) <- c(
+  "fte_cod", "fte",
+  "despesa",
+  "valor_desp"
+)
+
+desp %>% group_by(despesa) %>% summarise(sum(valor_desp))
+
+sum(rec$valor_rec) - sum(desp$valor_desp) # tem que dar zero, ou próximo
+
+# 02 Preparação Matriz --------------------------------------------------
+
+rec_dist <- rec %>%
+  group_by(receita) %>%
+  mutate(subtot_rec = sum(valor_rec),
+         pct_rec_fte = valor_rec/sum(valor_rec)) %>%
+  select(receita, fte, subtot_rec, pct_rec_fte)
+
+desp_dist <- desp %>%
+  group_by(fte) %>%
+  mutate(pct_fte_des = valor_desp/sum(valor_desp)) %>%
+  select(despesa, fte, pct_fte_des)
+
+matriz_raw <- rec_dist %>%
+  full_join(desp_dist, by = "fte")
+
+matriz <- matriz_raw %>%
+  mutate(pct_rec_desp = pct_rec_fte * pct_fte_des,
+         valor_link   = subtot_rec * pct_rec_desp) %>%
+  filter(!is.na(receita)) %>%
+  group_by(receita, despesa) %>%
+  summarise(pct_rec_desp = sum(pct_rec_desp),
+            valor_link   = sum(valor_link))
+
+# verifica
+sum(matriz$valor_link, na.rm = T) - sum(rec$valor_rec) # tem que dar zero
+
+# testes
+matriz %>% filter(valor_link  >= 2.5e8) %>% nrow()
+ggplot(matriz %>% filter(valor_link  <= 1e9)) + geom_histogram(aes(valor_link))
+
+# filtra
+matriz_reduz <- matriz %>% filter(
+  valor_link >= 2.5e8
+)
+
+sum(matriz_reduz$valor_link, na.rm = T)- sum(rec$valor_rec)
+
+
+# 03 Numeração dos nós ----------------------------------------------------
+
+# relacao unica dos rotulos de receita e despesa:
+rotulos <- c(unique(matriz_reduz$receita),unique(matriz_reduz$despesa))
+
+# conta os nós e gera sequencia numerica a partir de zero
+num_nodes <- length(rotulos)
+nodes_vetor <- 0:(num_nodes - 1)
+
+# cria tabelinha para numerar os nos
+tab_nodes <- data.frame(nodes_vetor, 
+                        rotulos)
+
+# incorpora os números dos nodes na matriz, para a receita... e para a despesa.
+matriz_nodes <- matriz_reduz %>%
+  left_join(tab_nodes, by = c("receita" = "rotulos")) %>%   
+  left_join(tab_nodes, by = c("despesa" = "rotulos"), suffix = c("_rec","_desp"))
+
+
+
+# 04 Prepara dados para D3 ------------------------------------------------
+
+links <- matriz_nodes %>%
+  select(source = receita,
+         target = despesa,
+         value  = valor_link)
+
+nodes <- tab_nodes
+nodes$tipo <- c(
+  rep("receita", length(unique(matriz_reduz$receita))),
+  rep("despesa", length(unique(matriz_reduz$despesa)))
+)
+
+write.csv(links, "./vis/links.csv", fileEncoding = "utf8")
+write.csv(nodes, "./vis/nodes.csv", fileEncoding = "utf8")
+
---
+++ last/vis/utils.js
@@ -0,0 +1,212 @@
+const utils_consts = {
+
+  localeBrasil : {
+    "decimal": ",",
+    "thousands": ".",
+    "grouping": [3],
+    "currency": ["R$", ""]
+  },
+
+  //https://cdn.jsdelivr.net/npm/d3-time-format@2/locale/pt-BR.json
+  localeDataBrasil : {
+    "dateTime": "%A, %e de %B de %Y. %X",
+    "date": "%d/%m/%Y",
+    "time": "%H:%M:%S",
+    "periods": ["AM", "PM"],
+    "days": ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    "shortDays": ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    "months": ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+    "shortMonths": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+  },
+
+}
+
+const utils = {
+
+  localeBrasil : utils_consts.localeBrasil,
+  localeDataBrasil : utils_consts.localeDataBrasil,
+
+  formataBR   : d3.formatDefaultLocale(utils_consts.localeBrasil).format(",.0f"),
+  formataBR_1 : d3.formatDefaultLocale(utils_consts.localeBrasil).format(",.1f"),
+  formataPct  : d3.format(".0%"),
+
+  valor_formatado : function(x) { 
+
+    const multiplos = [1, 1e3, 1e6, 1e9, 1e12];
+    const sufixo    = ["", "mil", "mi", "bi", "tri"];
+    const obj_mult = multiplos.map((d,i) => ({
+      valor: d,
+      sufixo: sufixo[i]
+    }));
+
+    for (mult of obj_mult) {
+      const val = x/mult.valor;
+      if (val < 1000) return utils.formataBR_1(val) + " " + mult.sufixo;
+    }
+  },
+
+  formataData : d3.timeFormat("%b %Y"),
+
+  formataData_Anos : d3.timeFormat("%Y"),
+
+  group_by_sum_d3 : function(objeto, coluna_categoria, coluna_valor, ordena_decrescente = false) {
+    const resultado = []; 
+    const categorias_unicas = d3.map(objeto, d => d[coluna_categoria]).keys();
+    for (cat of categorias_unicas) {
+      const subtotal_categoria = d3.sum(objeto.filter(d => d[coluna_categoria] === cat), d => +d[coluna_valor]);
+      resultado.push({"categoria" : cat,
+                      "subtotal"  : subtotal_categoria});   
+    }
+    if (ordena_decrescente) resultado.sort((a,b) => b.subtotal - a.subtotal);
+    return resultado;
+  },
+
+  group_by_sum : function(objeto, coluna_categoria, coluna_valor, ordena_decrescente = false) {
+    const resultado = []; 
+    const categorias_unicas = objeto
+                                .map(d => d[coluna_categoria])
+                                .filter((v, i, a) => a.indexOf(v) === i);
+    for (cat of categorias_unicas) {
+      const soma = objeto
+                      .filter(d => d[coluna_categoria] === cat)
+                      .map(d => +d[coluna_valor])
+                      .reduce((valor_acum, valor_atual) => valor_acum + valor_atual);
+      resultado.push({"categoria" : cat,
+                      "subtotal"  : soma});   
+    }
+    if (ordena_decrescente) resultado.sort((a,b) => b.subtotal - a.subtotal)
+    return resultado;
+  },
+
+  unique : function(obj, col) {
+    return obj
+      .map(d => d[col])
+      .filter((v, i, a) => a.indexOf(v) === i);
+  },
+
+  group_and_count : function(obj, col, percent = false) {
+    let categories = utils.unique(obj, col);
+  
+    let divisor = percent ? obj.length : 1
+  
+    let result = categories.map(
+      cat => ({
+        "cat" : cat,
+        "count": obj
+          .filter(el => el[col] == cat)
+          .length / divisor
+      })
+    );
+  
+    return result;
+  },
+
+
+  // para gerar arco para anotações
+
+  gera_arco : function(x1,y1,x2,y2) {
+    xmin = Math.min(x1,x2);
+    xmax = Math.max(x1,x2);
+    ymin = Math.min(y1,y2);
+    xb = xmax - Math.abs(x2-x1)/6;
+    yb = ymin + Math.abs(y2-y1)/6;
+    path = "M" + x1 + "," + y1 + " Q" + xb + "," + yb + " " + x2 + "," + y2;
+    //console.log(path)
+    return path;
+  },
+
+
+  gera_grid : function(svg_ref, step = 10) {
+    const w = svg_ref.attr("width") | +svg_ref.style("width").slice(0,-2);
+    const h = svg_ref.attr("height") | +svg_ref.style("height").slice(0,-2);
+    console.log("svg dimensions", w, h);
+  
+    const grid_color = "limegreen";
+    const vertical_color = "tomato";
+    const horizontal_color = "dodgerblue";
+  
+    const selecao = svg_ref.append("g").classed("grid-help", true);
+
+    for (let tick = 0; tick <= w; tick += step) {
+      selecao.append("line")
+        .attr("x1", tick)
+        .attr("x2", tick)
+        .attr("y1", 0)
+        .attr("y2", h)
+        .attr("stroke-width", 1)
+        .attr("stroke", grid_color);
+      selecao.append("text")
+        .attr("x", tick)
+        .attr("y", step)
+        .text(tick)
+        .attr("font-size", 8)
+        .attr("font-weight", 100)
+        .attr("fill", horizontal_color);
+      selecao.append("text")
+        .attr("x", tick)
+        .attr("y", 2*step)
+        .text(Math.round(100*tick/w, 0) + "w")
+        .attr("font-size", 8)
+        .attr("font-weight", 100)
+        .attr("fill", horizontal_color);
+    }
+    for (let tick = 0; tick <= h; tick += step) {
+      selecao.append("line")
+        .attr("x1", 0)
+        .attr("x2", w)
+        .attr("y1", tick)
+        .attr("y2", tick)
+        .attr("stroke-width", 1)
+        .attr("stroke", "lime");
+      selecao.append("text")
+        .attr("x", step)
+        .attr("y", tick + 8)
+        .text(tick)
+        .attr("font-size", 8)
+        .attr("text-anchor", "end")
+        .attr("fill", vertical_color);
+      selecao.append("text")
+        .attr("x", step*2)
+        .attr("y", tick + 8)
+        .text(Math.round(100*tick/h, 0) + "h")
+        .attr("font-size", 8)
+        .attr("text-anchor", "end")
+        .attr("fill", vertical_color);
+    }
+  },
+
+  // função debounce para dar um atraso na chamada do resize
+// https://davidwalsh.name/function-debounce
+  debounce : function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  },
+
+  init : function() {
+
+    d3.timeFormatDefaultLocale(utils_consts.localeDataBrasil);
+
+  }
+
+}
+
+utils.init();
+
+/* ideia para filtrar valores de uma lista 
+
+var arr = [{n: 1, p: "n"},{n: 2, p: "s"},{n: 3, p: "n"},{n: 4, p: "s"},{n: 4, p: "s"}],
+    brr = [2,4],
+    res = arr.filter(f => brr.includes(f.n));
+console.log(res);
+
+*/
